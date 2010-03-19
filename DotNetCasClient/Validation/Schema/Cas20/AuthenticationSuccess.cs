1d11c7a0828c5f164d308c25cb71b0a349ebfa6e
Scott Holodak <scottt732@gmail.com>
2010-03-19 15:33:52 +0000

    NETC-14 - Generated C# code from schemas using xsd.exe, cleaned up code & renamed classes.

---
+++ DotNetCasClient/Validation/Schema/Cas20/AuthenticationSuccess.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Xml.Serialization;
+
+namespace DotNetCasClient.Validation.Schema.Cas20
+{
+    [Serializable]
+    [DebuggerStepThrough]
+    [DesignerCategory("code")]
+    [XmlType(Namespace = "http://www.yale.edu/tp/cas")]
+    public class AuthenticationSuccess
+    {
+        internal AuthenticationSuccess() { }
+
+        [XmlElement("user")]
+        public string User
+        {
+            get;
+            set;
+        }
+
+        [XmlElement("proxyGrantingTicket")]
+        public string ProxyGrantingTicket
+        {
+            get;
+            set;
+        }
+
+        [XmlArray("proxies")]
+        [XmlArrayItem("proxy", IsNullable = false)]
+        public string[] Proxies
+        {
+            get;
+            set;
+        }
+    }
+}
+
+
---
+++ DotNetCasClient/Validation/Schema/Cas20/ProxyFailure.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Xml.Serialization;
+
+namespace DotNetCasClient.Validation.Schema.Cas20
+{
+    [Serializable]
+    [DebuggerStepThrough]
+    [DesignerCategory("code")]
+    [XmlType(Namespace="http://www.yale.edu/tp/cas")]
+    public class ProxyFailure {
+        internal ProxyFailure() { }
+        
+        [XmlAttribute("code")]
+        public string Code {
+            get;
+            set;
+        }
+        
+        [XmlText]
+        public string Message {
+            get;
+            set;
+        }
+    }
+}
