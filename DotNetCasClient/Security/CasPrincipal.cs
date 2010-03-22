8d70143f222351676ddcfa4dd25b9b4711471ef5
Scott Holodak <scottt732@gmail.com>
2010-03-22 17:04:50 +0000

    NETC-9, NETC-16 - Misc code cleanup, formatting, and documentation - Re-implement proxy ticket generation, implement proxy ticket authentication

--- DotNetCasClient/Security/CasPrincipal.cs
+++ DotNetCasClient/Security/CasPrincipal.cs
@@ -1,8 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Security.Principal;
-using DotNetCasClient.Proxy;
 using DotNetCasClient.Utils;
-using log4net;
 
 namespace DotNetCasClient.Security
 {
@@ -18,59 +17,43 @@ namespace DotNetCasClient.Security
     [Serializable]
     public class CasPrincipal : ICasPrincipal
     {
-        /// <summary>
-        /// Access to the log file
-        /// </summary>
-        static readonly ILog log = LogManager.GetLogger(
-          System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-
         #region ICasPrincipal Members
-
-        public string GetProxyTicketFor(Uri service)
-        {
-            if (this.proxyGrantingTicket != null)
-            {
-                return this.proxyRetriever.GetProxyTicketIdFor(
-                  this.proxyGrantingTicket, service);
-            }
-            log.Debug(string.Format("{0}:" +
-              "No ProxyGrantingTicket was supplied --> returning null",
-              CommonUtils.MethodName));
-            return null;
-        }
-
         /// <summary>
         /// The Assertion backing this Principal
         /// </summary>
-        public IAssertion Assertion { get; private set; }
-        
+        public IAssertion Assertion
+        {
+            get;
+            private set;
+        }
+
+        public string ProxyGrantingTicket
+        {
+            get;
+            private set;
+        }
+
+        public IEnumerable<string> Proxies
+        {
+            get;
+            private set;
+        }
         #endregion
 
         # region IPrincipal Members
 
         // IIdentity associated with this IPrincipal
-        public IIdentity Identity { get; private set;}
-        
-        public bool IsInRole(string role)
+        public IIdentity Identity
         {
-            // TODO answer this with query to Attributes???
-            throw new NotImplementedException();
+            get;
+            private set;
         }
 
-        #endregion
-
-        # region Fields
-
-        /// <summary>
-        /// The CAS 2 ticket used to retrieve a proxy ticket
-        /// </summary>
-        string proxyGrantingTicket;
-
-        /// <summary>
-        /// The method to retrieve a proxy ticket from a CAS server.
-        /// </summary>
-        IProxyRetriever proxyRetriever;
-
+        public bool IsInRole(string role)
+        {
+            // TODO answer this with query to Attributes???)
+            throw new NotImplementedException();
+        }
         #endregion
 
         #region Constructors
@@ -80,8 +63,7 @@ namespace DotNetCasClient.Security
         /// <param name="assertion">
         /// the Assertion that backs this Principal
         /// </param>
-        public CasPrincipal(IAssertion assertion) : this(assertion, null, null)
-        { }
+        public CasPrincipal(IAssertion assertion) : this(assertion, null, null) { }
 
         /// <summary>
         /// Constructs a new Principal backed by the supplied Assertion, with
@@ -93,22 +75,38 @@ namespace DotNetCasClient.Security
         /// <param name="proxyGrantingTicket">
         /// the proxy granting ticket associated with this Principal.
         /// </param>
-        /// <param name="proxyRetriever">
-        /// the ProxyRetriever to call back to the CAS server.
+        public CasPrincipal(IAssertion assertion, string proxyGrantingTicket) : this(assertion, proxyGrantingTicket, null) { }
+
+        /// <summary>
+        /// Constructs a new Principal backed by the supplied Assertion, with
+        /// proxying capabilities.
+        /// </summary>
+        /// <param name="assertion">
+        /// the Assertion that backs this Principal
         /// </param>
-        public CasPrincipal(IAssertion assertion, 
-                            string proxyGrantingTicket,
-                            IProxyRetriever proxyRetriever)
+        /// <param name="proxyGrantingTicket">
+        /// the proxy granting ticket associated with this Principal.
+        /// </param>
+        /// <param name="proxies">
+        /// The proxy path associated with this Principal
+        /// </param>
+        public CasPrincipal(IAssertion assertion, string proxyGrantingTicket, IEnumerable<string> proxies)
         {
             CommonUtils.AssertNotNull(assertion, "assertion cannot be null.");
 
-            this.Identity = new GenericIdentity(assertion.PrincipalName,
-              CommonUtils.CAS_AUTH_TYPE);
-            this.Assertion = assertion;
-            this.proxyGrantingTicket = proxyGrantingTicket;
-            this.proxyRetriever = proxyRetriever;
-        }
+            Identity = new GenericIdentity(assertion.PrincipalName, CommonUtils.CAS_AUTH_TYPE);
+            Assertion = assertion;
+            ProxyGrantingTicket = proxyGrantingTicket;
 
+            if (proxies != null)
+            {
+                Proxies = proxies;
+            }
+            else
+            {
+                Proxies = new List<string>();
+            }
+        }
         #endregion
     }
 }
