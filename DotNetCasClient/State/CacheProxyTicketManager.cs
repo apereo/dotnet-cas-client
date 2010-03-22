393fb6bbfa203e383fe3c69cc31f7f6c2fb5e1d4
Scott Holodak <scottt732@gmail.com>
2010-03-22 16:55:01 +0000

    NETC-7 Implement a modular server-side ticket storage framework

---
+++ DotNetCasClient/State/CacheProxyTicketManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Web;
+using System.Web.Caching;
+
+namespace DotNetCasClient.State
+{
+    public class CacheProxyTicketManager : IProxyTicketManager
+    {
+        private static readonly TimeSpan DefaultExpiration = new TimeSpan(0, 0, 3, 0); // 180 seconds
+
+        public void Initialize()
+        {
+            // Do nothing
+        }
+
+        /// <summary>
+        /// Removes expired entries from the ticket store
+        /// </summary>
+        public void RemoveExpiredMappings()
+        {
+            // No-op.  ASP.NET Cache provider removes expired entries automatically.
+        }
+
+        /// <summary>
+        /// Method to save the ProxyGrantingTicket to the backing storage facility.
+        /// </summary>
+        /// <param name="proxyGrantingTicketIou">used as the key</param>
+        /// <param name="proxyGrantingTicket">used as the value</param>
+        public void InsertProxyGrantingTicketMapping(string proxyGrantingTicketIou, string proxyGrantingTicket)
+        {
+            HttpContext.Current.Cache.Insert(proxyGrantingTicketIou, proxyGrantingTicket, null, DateTime.Now.Add(DefaultExpiration), Cache.NoSlidingExpiration);
+        }
+
+        /// <summary>
+        /// Method to retrieve a ProxyGrantingTicket based on the
+        /// ProxyGrantingTicketIou.  Implementations are not guaranteed to
+        /// return the same result if retieve is called twice with the same 
+        /// proxyGrantingTicketIou.
+        /// </summary>
+        /// <param name="proxyGrantingTicketIou">used as the key</param>
+        /// <returns>the ProxyGrantingTicket Id or null if it can't be found</returns>
+        public string GetProxyGrantingTicket(string proxyGrantingTicketIou)
+        {
+            if (HttpContext.Current.Cache[proxyGrantingTicketIou] != null && HttpContext.Current.Cache[proxyGrantingTicketIou].ToString().Length > 0)
+            {
+                return HttpContext.Current.Cache[proxyGrantingTicketIou].ToString();
+            }
+
+            return null;
+        }
+    }
+}
---
+++ DotNetCasClient/State/IProxyTicketManager.cs
@@ -0,0 +1,35 @@
+﻿namespace DotNetCasClient.State
+{
+    public interface IProxyTicketManager
+    {
+        /// <summary>
+        /// You retrieve CasAuthentication properties in the constructor or else you will cause 
+        /// a StackOverflow.  CasAuthentication.Initialize() will call Initialize() on all 
+        /// relevant controls when its initialization is complete.  In Initialize(), you can 
+        /// retrieve properties from CasAuthentication.
+        /// </summary>
+        void Initialize();
+
+        /// <summary>
+        /// Removes expired PGTIOU-PGT  from the ticket store
+        /// </summary>
+        void RemoveExpiredMappings();
+
+        /// <summary>
+        /// Method to save the ProxyGrantingTicket to the backing storage facility.
+        /// </summary>
+        /// <param name="proxyGrantingTicketIou">used as the key</param>
+        /// <param name="proxyGrantingTicket">used as the value</param>
+        void InsertProxyGrantingTicketMapping(string proxyGrantingTicketIou, string proxyGrantingTicket);
+
+        /// <summary>
+        /// Method to retrieve a ProxyGrantingTicket based on the
+        /// ProxyGrantingTicketIou.  Implementations are not guaranteed to
+        /// return the same result if retieve is called twice with the same 
+        /// proxyGrantingTicketIou.
+        /// </summary>
+        /// <param name="proxyGrantingTicketIou">used as the key</param>
+        /// <returns>the ProxyGrantingTicket Id or null if it can't be found</returns>
+        string GetProxyGrantingTicket(string proxyGrantingTicketIou);
+    }
+}
