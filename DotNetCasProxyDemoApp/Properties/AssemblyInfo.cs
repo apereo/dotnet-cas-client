3bf126908d7a8e071d9991269d80645ec391664a
Scott Holodak <scottt732@gmail.com>
2010-07-09 20:53:30 +0000

    NETC-18 Add Licensing File and Header to Source Files - Changed license text to Apache 2 license

--- DotNetCasProxyDemoApp/Properties/AssemblyInfo.cs
+++ DotNetCasProxyDemoApp/Properties/AssemblyInfo.cs
@@ -1,5 +1,23 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿/*
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,9 +26,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DotNetCasProxyDemoApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DotNetCasProxyDemoApp")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCopyright("Copyright © 2007-2010 The JA-SIG Collaborative. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
