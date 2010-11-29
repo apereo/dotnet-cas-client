7ad4416c9ecbcebcb730e9215bc097dcbbdb38e5
Marvin S. Addison <marvin.addison@gmail.com>
2010-11-29 16:37:32 +0000

    NOJIRA Add licensing header and authorship information to logging classes.

--- DotNetCasClient/Logging/Logger.cs
+++ DotNetCasClient/Logging/Logger.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
 using System.Diagnostics;
 
 namespace DotNetCasClient.Logging
@@ -6,6 +24,7 @@ namespace DotNetCasClient.Logging
     /// <summary>
     /// Simple logger implementation that insulates application from details of logging framework/strategy.
     /// </summary>
+    /// <author>Marvin S. Addison</author>
     public class Logger
     {
         private const int EVENT_ID = 0xCA5;
