a3489df6be8cf5ba794a8dfbad9a5c19a34eceb2
Marvin S. Addison <marvin.addison@gmail.com>
2012-03-08 16:48:04 -0500

    Update build for git

---
+++ DotNetCasClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DotNetCasClient")]
+[assembly: AssemblyDescription("CAS client integration library for the .NET Framework.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Jasig")]
+[assembly: AssemblyProduct("DotNetCasClient")]
+[assembly: AssemblyCopyright("Copyright © 2007-2010 Jasig.  All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("112fbe82-5287-411f-96c7-cfd3bab1ebd3")]
+[assembly: AssemblyVersion("1.0.2.*")]
+[assembly: AssemblyFileVersion("1.0.2.*")]
