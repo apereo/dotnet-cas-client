cd80d57cd3cc24331171da55adf404cb9ccda165
Jason <phantomtypist@hotmail.com>
2017-07-11 23:54:56 -0400

    - Pulling down nuget.exe in build.cmd instead of relying on it already being tracked in the repo tools folder. - Pulling down specific version of FAKE 4.x.  I want to avoid version 5 whenever it comes out of pre-release because of possible breaking changes.

--- build.cmd
+++ build.cmd
@@ -2,16 +2,16 @@
 
 cls
 
-:: Update NuGet executable.
-tools\nuget\nuget.exe u -self
+:: Install NuGet
+echo Downloading NuGet...
+PowerShell.exe -Command "New-Item -ItemType Directory -Force -Path 'tools\nuget' | out-null"
+PowerShell.exe -Command "Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile tools\nuget\nuget.exe"
+echo Downloaded NuGet!
 
-:: Build
-cls
-
-:: Install FAKE.
-"tools\nuget\nuget.exe" install "FAKE" -source "https://nuget.org/api/v2/" -OutputDirectory "tools" -ExcludeVersion
-
-cls
+:: Install FAKE
+echo Downloading FAKE...
+"tools\nuget\nuget.exe" install "FAKE" -Version "4.62.1" -source "https://nuget.org/api/v2/" -OutputDirectory "tools" -ExcludeVersion
+echo Downloaded FAKE!
 
 :: Run the FAKE build script.
 "tools\FAKE\tools\Fake.exe" build.fsx %*
