cd3f0052bdb5d5083453c52aaf880d4b5f93b4b6
Jason Kanaris <phantomtypist@hotmail.com>
2019-03-30 23:55:15 -0400

    - Added missing license comments. - Disabled CS1591 compiler warning on certain classes.

--- DotNetCasClient/Properties/AutoGeneratedAssemblyInfo.cs
+++ DotNetCasClient/Properties/AutoGeneratedAssemblyInfo.cs
@@ -11,6 +11,6 @@ using System.Reflection;
 [assembly: AssemblyProduct("Apereo .NET CAS Client")]
 [assembly: AssemblyVersion("1.2.0.0")]
 [assembly: AssemblyFileVersion("1.2.0.0")]
-[assembly: AssemblyInformationalVersion("1.2.0-issue-64.1+18.Branch.feature/issue-64.Sha.ec629203a46230f2987bd5b4ec464bc3b70a2fec")]
+[assembly: AssemblyInformationalVersion("1.2.0-issue-64.1+19.Branch.feature/issue-64.Sha.8a3fb63f325ca0ed71291bf1bee23554a2f46a5f")]
 [assembly: AssemblyCopyright("Copyright (c) 2007-2019 Apereo.  All rights reserved.")]
 
