7d20b091e5832ef68ef7ad00934eeddc3f751d9a
Jason Kanaris <phantomtypist@hotmail.com>
2019-03-31 23:41:43 -0400

    Changed code as per issue #64 so that the during a SAML 1.1 ticket validation the SAML Request values for RequestID and IssueInstant are no longer hardcoded.

--- DotNetCasClient/Properties/AutoGeneratedAssemblyInfo.cs
+++ DotNetCasClient/Properties/AutoGeneratedAssemblyInfo.cs
@@ -11,6 +11,6 @@ using System.Reflection;
 [assembly: AssemblyProduct("Apereo .NET CAS Client")]
 [assembly: AssemblyVersion("1.2.0.0")]
 [assembly: AssemblyFileVersion("1.2.0.0")]
-[assembly: AssemblyInformationalVersion("1.2.0-issue-64.1+19.Branch.feature/issue-64.Sha.8a3fb63f325ca0ed71291bf1bee23554a2f46a5f")]
+[assembly: AssemblyInformationalVersion("1.2.0-issue-64.1+20.Branch.feature/issue-64.Sha.cd3f0052bdb5d5083453c52aaf880d4b5f93b4b6")]
 [assembly: AssemblyCopyright("Copyright (c) 2007-2019 Apereo.  All rights reserved.")]
 
